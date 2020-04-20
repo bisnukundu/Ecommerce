8362fd3c2fb1da44c158791358af5f51b3a2e4bf
bisnukundu <bisnukundu@gmail.com>
2020-04-20 21:45:11 +0600

    Ecommerce Admin-panel

---
+++ admin/sidebar.php
@@ -0,0 +1,67 @@
+<div class="page-header">
+    <!-- LEFTSIDE header -->
+    <div class="leftside-header">
+        <div class="logo">
+            <a href="index.html" class="on-click">
+                <img alt="logo" src="images/header-logo.png" />
+            </a>
+        </div>
+        <div id="menu-toggle" class="visible-xs toggle-left-sidebar" data-toggle-class="left-sidebar-open" data-target="html">
+            <i class="fa fa-bars" aria-label="Toggle sidebar"></i>
+        </div>
+    </div>
+    <!-- ========================================================= -->
+    <div class="page-body">
+        <!-- LEFT SIDEBAR -->
+        <!-- ========================================================= -->
+        <div class="left-sidebar">
+            <!-- left sidebar HEADER -->
+            <div class="left-sidebar-header">
+                <div class="left-sidebar-title">Navigation</div>
+                <div class="left-sidebar-toggle c-hamburger c-hamburger--htla hidden-xs" data-toggle-class="left-sidebar-collapsed" data-target="html">
+                    <span></span>
+                </div>
+            </div>
+            <!-- NAVIGATION -->
+            <!-- ========================================================= -->
+            <div id="left-nav" class="nano">
+                <div class="nano-content">
+                    <nav>
+                        <ul class="nav nav-left-lines" id="main-nav">
+                            <!--HOME-->
+                            <li class="active-item"><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i><span>Dashboard</span></a></li>
+                            <li class="has-child-item close-item">
+                                <a><i class="fa fa-columns" aria-hidden="true"></i><span>Forms </span></a>
+                                <ul class="nav child-nav level-1">
+                                    <li><a href="forms_layouts.html">Layouts</a></li>
+                                    <li><a href="forms_elements.html">Elements</a></li>
+                                    <li><a href="forms_advanced.html">Advanced</a></li>
+                                    <li><a href="forms_validation.html">Validation</a></li>
+                                    <li><a href="forms_wizard.html">Wizard</a></li>
+                                </ul>
+                            </li>
+                            <!--TABLES-->
+                            <li class="has-child-item close-item">
+                                <a><i class="fa fa-table" aria-hidden="true"></i><span>Tables</span></a>
+                                <ul class="nav child-nav level-1">
+                                    <li><a href="tables_basic.html">Basic</a></li>
+                                    <li><a href="tables_data-tables.html">DataTable</a></li>
+                                    <li><a href="tables_responsive.html">Responsive</a></li>
+                                </ul>
+                            </li>
+                            <li class="has-child-item close-item">
+                                <a><i class="fa fa-table" aria-hidden="true"></i><span>Slider</span></a>
+                                <ul class="nav child-nav level-1">
+                                    <li><a href="tables_basic.html">Basic</a></li>
+                                    <li><a href="tables_data-tables.html">DataTable</a></li>
+                                    <li><a href="tables_responsive.html">Responsive</a></li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ cart.php
@@ -0,0 +1,4 @@
+<?php
+    $dat = $_REQUEST["data"];
+   echo "<img src='$dat[0]' alt='cart_img'/>";
+?>
